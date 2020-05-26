c3d34998721f3b8ef2047d93d2f7e23d6e1f4532
Oliver <draper87@gmail.com>
2020-05-26 15:59:34 +0200

    Fix

--- js/script.js
+++ js/script.js
@@ -14,27 +14,17 @@ arrayCognomi.push(cognomeUtente);
 // Sistemo i cognomi per ordine alfabetico
 var arrayCognomiOrdinati = arrayCognomi.sort();
 
-
 // Creo accesso al DOM
 var lista = document.getElementById('lista');
 var posizione = document.getElementById('posizione');
 
-var numeroDellaLista;
-
 // Stampo i cognomi in ordine alfabetico
-
 for (var i = 0; i < arrayCognomiOrdinati.length; i++) {
   var listaDiAppoggio = lista.innerHTML;
   lista.innerHTML = listaDiAppoggio + '<li>' + arrayCognomiOrdinati[i] + '</li>';
 }
 
 // Stampo la posizione del cognomeUtente all interno della lista
-// i = 0;
-// while (arrayCognomiOrdinati[i] == cognomeUtente ) {
-//   console.log(arrayCognomiOrdinati[i]);
-//   posizione.innerHTML = 'La posizione del tuo cognome nella lista è' +
-//   i++;
-// }
 for (var i = 0; i < arrayCognomiOrdinati.length; i++) {
   if (arrayCognomiOrdinati[i] == cognomeUtente) {
     posizione.innerHTML = 'Il tuo cognome è il numero ' + (i + 1) + ' nella lista';
